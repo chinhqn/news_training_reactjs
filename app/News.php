bd14ae46a2aff2d8d9171aaae30b21ca45e7d83e
chinhqn <chinhsoftoneqn@gmail.com>
2018-04-13 18:06:24 +0700

    create repo

---
+++ app/News.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = ['name', 'preview_text', 'detail_text', 'image'];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            NewsTableSeeder::class,
+        
+        ]);
     }
+
+
+    
+    
 }
+
