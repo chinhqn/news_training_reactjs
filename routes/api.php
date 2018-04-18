072aa6859339939a319f95e576fc0c1dc17b9e31
chinhqn <chinhsoftoneqn@gmail.com>
2018-04-18 17:10:53 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
+
 Route::resource('cats', CatsController::class);
 Route::resource('news', NewsController::class);
 
-
-
-=======
 Route::resource('user', UsersController::class);
 Route::resource('news', NewsController::class);
 Route::resource('cat', CatsController::class);
->>>>>>> daf04b75fe68b069f9cbbd59f5649a9bc0b25381
+
